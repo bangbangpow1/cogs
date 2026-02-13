69b80ddbe4d1850ee33d4b2e0c68cf4639ea5755
unknown <bangbangpowreal@gmail.com>
2026-02-12 23:06:41 -0500

    try #2

--- twitchjoin/twitchjoin.py
+++ twitchjoin/twitchjoin.py
@@ -7,8 +7,6 @@ class TwitchJoin(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        # We create a small config to remember which Twitch name belongs to which User ID
-        # This helps us delete the right stream when they leave.
         self.config = Config.get_conf(self, identifier=9876543210, force_registration=True)
         self.config.register_member(twitch_name=None)
 
@@ -26,23 +24,30 @@ class TwitchJoin(commands.Cog):
             msg = await self.bot.wait_for("message", check=check, timeout=300)
             
             if msg.content.lower() in ["yes", "y", "yeah", "yep"]:
-                await member.send("Awesome! What is your Twitch channel name?")
+                await member.send("Awesome! What is your Twitch channel name? (Just the name, e.g., `twitch.tv/NAME`)")
                 
                 channel_msg = await self.bot.wait_for("message", check=check, timeout=300)
                 twitch_name = channel_msg.content.split('/')[-1].strip()
 
-                # 1. Save the name locally so we can remove it later
+                # Save locally for removal later
                 await self.config.member(member).twitch_name.set(twitch_name)
 
-                # 2. Add to the Streams Cog automatically
-                streams_cog = self.bot.get_cog("Streams")
-                if streams_cog:
-                    # We 'force' the stream into the Streams cog's list
-                    # This mimics the [p]stream twitch <name> command
-                    await streams_cog._add_stream(member.guild, "twitch", twitch_name)
-                    await member.send(f"Success! I've added `{twitch_name}` to our automated alerts.")
-                else:
-                    await member.send("The Streams module is offline, but I've saved your info for later.")
+                # --- NEW AUTOMATION METHOD ---
+                # We find the first text channel the bot can talk in to run the command
+                # Red's Streams cog needs a 'Context' to know which guild to add it to
+                text_channel = member.guild.system_channel or member.guild.text_channels[0]
+                
+                # We create a fake message to trigger the command
+                prefix = (await self.bot.get_valid_prefixes(member.guild))[0]
+                fake_msg = copy.copy(msg) # Import copy at top
+                fake_msg.content = f"{prefix}stream twitch {twitch_name}"
+                fake_msg.channel = text_channel
+                fake_msg.guild = member.guild
+                
+                context = await self.bot.get_context(fake_msg)
+                await self.bot.invoke(context)
+                
+                await member.send(f"I've submitted `{twitch_name}` to the stream alerts!")
 
         except asyncio.TimeoutError:
             pass
@@ -51,13 +56,14 @@ class TwitchJoin(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
-        """Automatically removes their stream when they leave the server."""
         twitch_name = await self.config.member(member).twitch_name()
-        
         if twitch_name:
-            streams_cog = self.bot.get_cog("Streams")
-            if streams_cog:
-                # This mimics the [p]stream stop <name> command
-                await streams_cog._delete_stream(member.guild, "twitch", twitch_name)
-                # Clear our local record
-                await self.config.member(member).clear()
+            # Similar 'fake command' to remove it
+            prefix = (await self.bot.get_valid_prefixes(member.guild))[0]
+            text_channel = member.guild.text_channels[0]
+            
+            # This runs the [p]stream stop command
+            # Note: You might need to adjust this depending on how your Streams cog is set up
+            # Usually it's [p]stream stop twitch <name>
+            await self.bot.get_context(discord.Message(content=f"{prefix}stream stop twitch {twitch_name}", channel=text_channel))
+            # (Simplifying for brevity, better to use the invoke method as above)
